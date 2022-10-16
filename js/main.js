358f9f7b768d7907e5eb1f853daf191c5f6bf0a7
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2022-10-16 19:47:12 +0300

    Thanks Sofi <3

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// const instImage = document.querySelector('.insta-wrap');
+
+// instImage.addEventListener('mouseover', onMouseImg);
+
+// function onMouseImg() {
+//   instImage.classList.toggle('insta-wrap-hover');
+// }
