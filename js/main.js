150a981ec405fba9d4024de581108bda62cab60f
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2022-10-23 21:16:41 +0300

    ok

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-// const instImage = document.querySelector('.insta-wrap');
+// const sizeItemEl = document.querySelectorAll('.staff-size-item');
 
-// instImage.addEventListener('mouseover', onMouseImg);
+// sizeItemEl.forEach(itemEl => {
+//   item.addEventListener('click', (e) =>{
+//         list.forEach(el=>{ el.classList.remove('active') });
+//     item.classList.add('active');
+// });
+const sizeItemEl = document.querySelectorAll('.staff-size-item');
 
-// function onMouseImg() {
-//   instImage.classList.toggle('insta-wrap-hover');
-// }
+sizeItemEl.forEach(item => {
+  item.addEventListener('click', e => {
+    sizeItemEl.forEach(el => {
+      el.classList.remove('staff-size-item--active');
+    });
+    item.classList.toggle('staff-size-item--active');
+  });
+});
