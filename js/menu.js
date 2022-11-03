c61af0da6460a1719b6e1fd49d75cee111632930
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2022-11-04 00:19:22 +0200

    uhh

--- js/menu.js
+++ js/menu.js
@@ -12,4 +12,11 @@
     document.body.classList.toggle('no-skroll');
     refs.menu.classList.toggle('is-hidden');
   }
+  // refs.openMenuBtn.addEventListener('click', toggleMenu);
+  // refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // function toggleMenu() {
+  //   document.body.classList.toggle('no-skroll');
+  //   refs.menu.classList.toggle('is-hidden');
+  // }
 })();
